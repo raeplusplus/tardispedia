5b6c0390f871b8d99fc451806017d60c8c604e45
Raeshelle Rose <raeshellerose@outlook.com>
2022-05-16 01:22:47 -0500

    updated html on indiviual pages, thinking through logic on API calls

--- assets/js/api-commands.js
+++ assets/js/api-commands.js
@@ -1,6 +1,147 @@
 
 // Catalogopolis API
 
+{//random doctor button
+//event listener, on click
+//take to a random page
+if(randomDoc === 1){
+    //take to this page: 1st-doctor.html
+}else if(randomDoc === 2){
+    //take to this page: 2nd-doctor.html
+}else if(randomDoc === 3){
+    //take to this page: 3rd-doctor.html
+}else if(randomDoc === 4){
+    //take to this page: 4th-doctor.html
+}else if(randomDoc === 5){
+    //take to this page: 5th-doctor.html
+}else if(randomDoc === 6){
+    //take to this page: 6th-doctor.html
+}else if(randomDoc === 7){
+    //take to this page: 7th-doctor.html
+}else if(randomDoc === 8){
+    //take to this page: 8th-doctor.html
+}else if(randomDoc === 9){
+    //take to this page: war-doctor.html
+}else if(randomDoc === 10){
+    //take to this page: 9th-doctor.html
+}else if(randomDoc === 11){
+    //take to this page: 10th-doctor.html
+}else if(randomDoc === 12){
+    //take to this page: 11th-doctor.html
+}else if(randomDoc === 13){
+    //take to this page: 12th-doctor.html
+}else if(randomDoc === 14){
+    //take to this page: 13th-doctor.html
+}else if(randomDoc === 15){
+    //take to this page: 14th-doctor.html
+}else if(randomDoc === 16){
+    //take to this page: ruth-doctor.html
+}
+}
 
+{//on doctor page, random episode button
+//first, find which doctor it is
+if(chosenDoctor === "The First Doctor"){
+    //then a list of the episodes the doctor appears in:
+    if(randomFirstDocEpisode === 0){
+        //then, append this specific youtube embed to the html video source tag:
+    }else if(randomFirstDocEpisode === 0){
+        //then, append this specific youtube embed to the html video source tag:
+    }else if(randomFirstDocEpisode === 0){
+        //then, append this specific youtube embed to the html video source tag:
+    }else if(randomFirstDocEpisode === 0){
+        //then, append this specific youtube embed to the html video source tag:
+    }else if(randomFirstDocEpisode === 0){
+        //then, append this specific youtube embed to the html video source tag:
+    }else if(randomFirstDocEpisode === 0){
+        //then, append this specific youtube embed to the html video source tag:
+    }else if(randomFirstDocEpisode === 0){
+        //then, append this specific youtube embed to the html video source tag:
+    }else if(randomFirstDocEpisode === 0){
+        //then, append this specific youtube embed to the html video source tag:
+    }else if(randomFirstDocEpisode === 0){
+        //then, append this specific youtube embed to the html video source tag:
+    }else if(randomFirstDocEpisode === 0){
+        //then, append this specific youtube embed to the html video source tag:
+    }else if(randomFirstDocEpisode === 0){
+        //then, append this specific youtube embed to the html video source tag:
+    }else if(randomFirstDocEpisode === 0){
+        //then, append this specific youtube embed to the html video source tag:
+    }else if(randomFirstDocEpisode === 0){
+        //then, append this specific youtube embed to the html video source tag:
+    }else if(randomFirstDocEpisode === 0){
+        //then, append this specific youtube embed to the html video source tag:
+    }else if(randomFirstDocEpisode === 0){
+        //then, append this specific youtube embed to the html video source tag:
+    }else if(randomFirstDocEpisode === 0){
+        //then, append this specific youtube embed to the html video source tag:
+    }else if(randomFirstDocEpisode === 0){
+        //then, append this specific youtube embed to the html video source tag:
+    }else if(randomFirstDocEpisode === 0){
+        //then, append this specific youtube embed to the html video source tag:
+    }else if(randomFirstDocEpisode === 0){
+        //then, append this specific youtube embed to the html video source tag:
+    }else if(randomFirstDocEpisode === 0){
+        //then, append this specific youtube embed to the html video source tag:
+    }else if(randomFirstDocEpisode === 0){
+        //then, append this specific youtube embed to the html video source tag:
+    }else if(randomFirstDocEpisode === 0){
+        //then, append this specific youtube embed to the html video source tag:
+    }else if(randomFirstDocEpisode === 0){
+        //then, append this specific youtube embed to the html video source tag:
+    }
 
+}else if(chosenDoctor === "The Second Doctor"){
+    //then a list of the episodes the doctor appears in:
+    if(randomSecondDocEpisode === 0){
+        //then, append this specific youtube embed to the html video source tag:
+    }else if(randomSecondDocEpisode === 0){
+        //then, append this specific youtube embed to the html video source tag:
+    }else if(randomSecondDocEpisode === 0){
+        //then, append this specific youtube embed to the html video source tag:
+    }else if(randomSecondDocEpisode === 0){
+        //then, append this specific youtube embed to the html video source tag:
+    }else if(randomSecondDocEpisode === 0){
+        //then, append this specific youtube embed to the html video source tag:
+    }else if(randomSecondDocEpisode === 0){
+        //then, append this specific youtube embed to the html video source tag:
+    }else if(randomSecondDocEpisode === 0){
+        //then, append this specific youtube embed to the html video source tag:
+    }else if(randomSecondDocEpisode === 0){
+        //then, append this specific youtube embed to the html video source tag:
+    }else if(randomSecondDocEpisode === 0){
+        //then, append this specific youtube embed to the html video source tag:
+    }else if(randomSecondDocEpisode === 0){
+        //then, append this specific youtube embed to the html video source tag:
+    }else if(randomSecondDocEpisode === 0){
+        //then, append this specific youtube embed to the html video source tag:
+    }else if(randomSecondDocEpisode === 0){
+        //then, append this specific youtube embed to the html video source tag:
+    }else if(randomSecondDocEpisode === 0){
+        //then, append this specific youtube embed to the html video source tag:
+    }else if(randomSecondDocEpisode === 0){
+        //then, append this specific youtube embed to the html video source tag:
+    }else if(randomSecondDocEpisode === 0){
+        //then, append this specific youtube embed to the html video source tag:
+    }else if(randomSecondDocEpisode === 0){
+        //then, append this specific youtube embed to the html video source tag:
+    }else if(randomSecondDocEpisode === 0){
+        //then, append this specific youtube embed to the html video source tag:
+    }else if(randomSecondDocEpisode === 0){
+        //then, append this specific youtube embed to the html video source tag:
+    }else if(randomSecondDocEpisode === 0){
+        //then, append this specific youtube embed to the html video source tag:
+    }else if(randomSecondDocEpisode === 0){
+        //then, append this specific youtube embed to the html video source tag:
+    }else if(randomSecondDocEpisode === 0){
+        //then, append this specific youtube embed to the html video source tag:
+    }else if(randomSecondDocEpisode === 0){
+        //then, append this specific youtube embed to the html video source tag:
+    }else if(randomSecondDocEpisode === 0){
+        //then, append this specific youtube embed to the html video source tag:
+    }
+
+}
+
+}
 // Doctor Who Quotes API
