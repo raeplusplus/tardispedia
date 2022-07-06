a27ce31723e56d33834519d3abcf3438e5545830
Raeshelle Rose <raeshellerose@outlook.com>
2022-07-06 15:55:26 -0500

    working out bug

--- assets/js/api-commands.js
+++ assets/js/api-commands.js
@@ -1,9 +1,17 @@
 
+function getRandomInt(min,max) 
+{//  return Math.floor(Math.random() * (max - min + 1)) + min;
+randomNum = Math.floor(Math.random() * (max - min + 1)) + min;
+return randomNum;
+} 
+
 document.querySelector('#randomDoctorButton').addEventListener('click', apiRequest)
 async function apiRequest(){
-    const doctorName = randomChosenDoctor().value
+    // let randomDoc = getRandomInt(0, 12).value
+    const doctorNum = getRandomInt(0, 12)
+    console.log(doctorNum)
     try{
-        const response = await fetch(`https://tardispedia-api.herokuapp.com/api/${doctorName}`)
+        const response = await fetch(`https://tardispedia-api.herokuapp.com/api/${doctorNum}`)
         const data = await response.json()
 
         console.log(data)
@@ -14,6 +22,8 @@ async function apiRequest(){
         document.getElementById('yearsActive').innerText = data.yearsActive
         document.getElementById('quote').innerText = data.quote
         document.getElementById('doctorImage').src = data.image
+    }catch(error){
+        console.log(error)
     }
 }
 
@@ -43,17 +53,17 @@ async function apiRequest(){
 function randomChosenDoctor(){
     console.log('hey, does this work, random doc?')
     let docRegen = [];
-    for(let i = 0; i < theDoctor[doctorName]; i++){
-        docRegen[i] = newDoctor();
-        docRegen[i].docName = theDoctor[i].docName;
+    // for(let i = 0; i < theDoctor[doctorName]; i++){
+    //     docRegen[i] = newDoctor();
+    //     docRegen[i].docName = theDoctor[i].docName;
        
-        document.querySelector('doctorName').innerText = preBuffer[i].docName
-        docRegen[i].mainActor = theDoctor[i].mainActor;
-        docRegen[i].altActor = theDoctor[i].altActor;
-        docRegen[i].yearsActive = theDoctor[i].yearsActive;
-        docRegen[i].firstAppearance = theDoctor[i].firstAppearance;
-        docRegen[i].companions = theDoctor[i].companions;
-    }
+    //     document.querySelector('doctorName').innerText = preBuffer[i].docName
+    //     docRegen[i].mainActor = theDoctor[i].mainActor;
+    //     docRegen[i].altActor = theDoctor[i].altActor;
+    //     docRegen[i].yearsActive = theDoctor[i].yearsActive;
+    //     docRegen[i].firstAppearance = theDoctor[i].firstAppearance;
+    //     docRegen[i].companions = theDoctor[i].companions;
+    // }
     let newDoctor = getRandomInt(0, docRegen.length - 1);
     let nd = newDoctor.length;
     let doctorName = document.getElementById('doctorName')
@@ -67,28 +77,23 @@ function randomChosenDoctor(){
     // let aa = altActor.length;
     let firstAppearance = document.getElementById('firstAppearance')
     // let fa = firstAppearance.length;
-    let yearsActive = document.getElementById('yearsActive')
+    // let yearsActive = document.getElementById('yearsActive')
     // let ya = yearsActive.length;
-    for(let p = 0; p < nd; p++){
-        doctorName[0].parentNode.removeChild(doctorName[0]);
-        doctorImage[0].parentNode.removeChild(doctorImage[0]);
-        mainActor[0].parentNode.removeChild(mainActor[0]);
-        altActor[0].parentNode.removeChild(altActor[0]);
-        firstAppearance[0].parentNode.removeChild(firstAppearance[0]);
-        yearsActive[0].parentNode.removeChild(yearsActive[0]);
-    }
+    // for(let p = 0; p < nd; p++){
+    //     doctorName[0].parentNode.removeChild(doctorName[0]);
+    //     doctorImage[0].parentNode.removeChild(doctorImage[0]);
+    //     mainActor[0].parentNode.removeChild(mainActor[0]);
+    //     altActor[0].parentNode.removeChild(altActor[0]);
+    //     firstAppearance[0].parentNode.removeChild(firstAppearance[0]);
+    //     yearsActive[0].parentNode.removeChild(yearsActive[0]);
+    // }
     
-    document.body.appendChild(newDoctor)
+    // document.body.appendChild(newDoctor)
 }
 
 
 
-function getRandomInt(min,max) 
-    {
-      //  return Math.floor(Math.random() * (max - min + 1)) + min;
-    randomNum = Math.floor(Math.random() * (max - min + 1)) + min;
-    return preBuffer[randomNum];
-    } 
+
 
 document.querySelector('#randomEpisodeButton').addEventListener('click', randomChosenDocEpisode)
 function randomChosenDocEpisode(){
